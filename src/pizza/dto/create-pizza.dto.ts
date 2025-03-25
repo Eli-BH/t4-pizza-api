0850a63ab9cdbb15c7081aff245e93d49b93083c
Eli-b-good <elijah@monalee.co>
2025-03-25 17:28:04 -0400

    FEAT: enhance CreatePizzaDto with validation for toppings, size, and sauce

--- src/pizza/dto/create-pizza.dto.ts
+++ src/pizza/dto/create-pizza.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePizzaDto {}
+import { IsNotEmpty, IsString, IsArray, IsEnum } from 'class-validator';
+import { SizeName } from '../entities/size.entity';
+import { SauceName } from '../entities/sauce.entity';
+import { ToppingName } from '../entities/topping.entity';
+
+export class CreatePizzaDto {
+  @IsArray()
+  @IsString({ each: true })
+  toppings: ToppingName[];
+
+  @IsNotEmpty()
+  @IsEnum(SizeName)
+  size: SizeName;
+
+  @IsNotEmpty()
+  @IsEnum(SauceName)
+  sauce: SauceName;
+}
