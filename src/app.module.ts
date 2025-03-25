75a10137b27fcab45a765d4f5a2a0e6eeaf72518
Eli-b-good <elijah@monalee.co>
2025-03-25 18:57:18 -0400

    FEAT: configure PostgreSQL database with TypeORM and update application structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PizzaModule } from './pizza/pizza.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PizzaModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'pizza_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // only for development
+    }),
+    PizzaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/pizza/pizza.module.ts
+++ src/pizza/pizza.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PizzaService } from './pizza.service';
 import { PizzaController } from './pizza.controller';
+import { Pizza } from './entities/pizza.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Sauce } from './entities/sauce.entity';
+import { Topping } from './entities/topping.entity';
+import { Size } from './entities/size.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Pizza, Sauce, Topping, Size])],
   controllers: [PizzaController],
   providers: [PizzaService],
 })
