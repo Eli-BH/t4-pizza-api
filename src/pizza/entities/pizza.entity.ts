58610a7ca0ab984db38a7c2aa41edd3e567b39ef
Eli-b-good <elijah@monalee.co>
2025-03-25 17:15:11 -0400

    FEAT: implement Pizza entity with relationships to Size, Sauce, and Topping

--- src/pizza/entities/pizza.entity.ts
+++ src/pizza/entities/pizza.entity.ts
@@ -1,1 +1,26 @@
-export class Pizza {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
+import { Size } from './size.entity';
+import { Sauce } from './sauce.entity';
+import { Topping } from './topping.entity';
+
+@Entity()
+export class Pizza {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Size)
+  size: Size;
+
+  @ManyToOne(() => Sauce)
+  sauce: Sauce;
+
+  @ManyToMany(() => Topping)
+  @JoinTable()
+  toppings: Topping[];
+}
